13d6db36c5a1de6211371250b864f9c6e7845cb3
Nicolas De Loof <nicolas.deloof@gmail.com>
2019-11-18 08:59:59 +0100

    Delete tag when PR is closed
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('child_process').spawnSync;
+const request = require('request');
 
 try {
   const username_input = core.getInput('username');
@@ -14,39 +15,63 @@ try {
   const sha = github.context.sha
   console.log(`sha: ${sha}`);
 
+  const repo = github.context.repo.owner + '/' + github.context.repo.repo
   var tag
   switch (github.context.eventName) {
       case 'pull_request':
           const pr = github.context.payload.number
-          tag = github.context.repo.owner + '/' + github.context.repo.repo + ':PR_' + pr
+          tag = ':PR_' + pr
           action = github.context.payload.action
           console.log(`Pull request #${pr} has been ${action}`);
           break;
       case 'push':
           const ref = github.context.ref
           // refs/heads/${branch} or refs/tags/${tag}
-          tag = github.context.repo.owner + '/' + github.context.repo.repo + ':' + ref.substring(ref.indexOf('/', 5)+1)
+          tag = ref.substring(ref.indexOf('/', 5)+1)
           break;
       default:
           console.log(`This action does not support ${github.context.eventName} events`);
   }
 
-  const loginProcess = exec('docker', [ 'login', '--username', username, '--password', password ]);
-  if (loginProcess.status !== 0) {
-    core.setFailed(`docker login failed with status code ${loginProcess.status}`);    
-  }
-  console.log(`Successfully logged in on DockerHub`);
 
+  if (github.context.action == 'closed') {
+    request({
+      uri: 'https://hub.docker.com/v2/users/login/',
+      method: 'POST',
+      json: { username: username, password: password }      
+    }, function (error, response, body) {  
+      console.log("logged in") 
+      const auth = `JWT ${body.token}`  
 
-  const buildProcess = exec('docker', [ 'build', '-t', tag, '.' ], { stdio: 'inherit' });
-  if (buildProcess.status !== 0) {
-    core.setFailed(`docker build failed with status code ${buildProcess.status}`);    
-  }
+      request({
+        uri: `https://hub.docker.com/v2/repositories/${repo}/tags/${tag}/`,
+        method: 'DELETE',
+        headers : {
+          "Authorization" : auth
+        }, function (error, response, body) { 
+          console.log("tag deleted") 
+        }
+      });
+    });
+  } else {
+    const loginProcess = exec('docker', [ 'login', '--username', username, '--password', password ]);
+    if (loginProcess.status !== 0) {
+      core.setFailed(`docker login failed with status code ${loginProcess.status}`);    
+    }
+    console.log(`Successfully logged in on DockerHub`);
   
-  const pushProcess = exec('docker', [ 'push', tag], { stdio: 'inherit' });
-  if (pushProcess.status !== 0) {
-    core.setFailed(`docker push failed with status code ${pushProcess.status}`);    
-  }
+    const buildProcess = exec('docker', [ 'build', '-t', repo+':'+tag, '.' ], { stdio: 'inherit' });
+    if (buildProcess.status !== 0) {
+      core.setFailed(`docker build failed with status code ${buildProcess.status}`);    
+    }
+    
+    const pushProcess = exec('docker', [ 'push', repo+':'+tag], { stdio: 'inherit' });
+    if (pushProcess.status !== 0) {
+      core.setFailed(`docker push failed with status code ${pushProcess.status}`);    
+    }  }
+
+
+
 
 } catch (error) {
   core.setFailed(error.message);
